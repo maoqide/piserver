9ba14813e333d3d27867d6b89097ff6d5180c870
maoqide <maoqiyx@163.com>
2017-05-02 14:53:32 +0800

    add motoctl_pwm to custom duty cycle

--- motoctl_pwm.py
+++ motoctl_pwm.py
@@ -1,9 +1,9 @@
 import RPi.GPIO as GPIO
 from time import sleep
 
-class moto():
+class Moto():
 
-    def __init__(self,out1,out2,out3,out4):
+    def __init__(self,out1,out2,out3,out4,ena,enb,freq=50):
         print "moto init..."
         GPIO.setmode(GPIO.BCM)
         GPIO.setwarnings(False)
@@ -11,6 +11,9 @@ class moto():
         self.out2 = out2
         self.out3 = out3
         self.out4 = out4
+        self.ena = ena
+        self.enb = enb
+        self.freq = freq
         self._setup()
 
     def _setup(self):
@@ -18,33 +21,50 @@ class moto():
         GPIO.setup(self.out2, GPIO.OUT)
         GPIO.setup(self.out3, GPIO.OUT)
         GPIO.setup(self.out4, GPIO.OUT)
+        GPIO.setup(self.ena, GPIO.OUT)
+        GPIO.setup(self.enb, GPIO.OUT)
+        GPIO.output(self.ena, 1)
+        GPIO.output(self.enb, 1)
+        Moto._pa=GPIO.PWM(self.ena, self.freq)
+        Moto._pb=GPIO.PWM(self.enb, self.freq)
 
     def stop(self):
+        Moto._pa.stop()
+        Moto._pb.stop()
         GPIO.output(self.out1, 0)
         GPIO.output(self.out2, 0)
         GPIO.output(self.out3, 0)
         GPIO.output(self.out4, 0)
 
-    def forward(self):
+    def forward(self, duty=50):
+        Moto._pa.start(duty)
+        Moto._pb.start(duty)
         GPIO.output(self.out1, 0)
         GPIO.output(self.out2, 1)
         GPIO.output(self.out3, 0)
         GPIO.output(self.out4, 1)
 
-    def backward(self):
+    def backward(self, duty=50):
+        Moto._pa.start(duty)
+        Moto._pb.start(duty)
         GPIO.output(self.out1, 1)
         GPIO.output(self.out2, 0)
         GPIO.output(self.out3, 1)
         GPIO.output(self.out4, 0)
 
-    def right(self):
-        GPIO.output(self.out1, 0)
-        GPIO.output(self.out2, 1)
-        GPIO.output(self.out3, 0)
-        GPIO.output(self.out4, 0)
-
-    def left(self):
+    def right(self, duty=50):
+        Moto._pb.start(duty)
+        #_pb.ChangeDutyCycle(duty)
         GPIO.output(self.out1, 0)
         GPIO.output(self.out2, 0)
         GPIO.output(self.out3, 0)
         GPIO.output(self.out4, 1)
+
+    def left(self, duty=50):
+        Moto._pa.start(duty)
+        #_pa.ChangeDutyCycle(duty)
+        GPIO.output(self.out1, 0)
+        GPIO.output(self.out2, 1)
+        GPIO.output(self.out3, 0)
+        GPIO.output(self.out4, 0)
+        
--- server.py
+++ server.py
@@ -1,7 +1,10 @@
 from flask import Flask,render_template,request,redirect,url_for
-import motoctl
+#import motoctl
+import motoctl_pwm
 app = Flask(__name__)
-moto = motoctl.moto(6, 13, 19, 26)
+#moto = motoctl.moto(6, 13, 19, 26)
+moto = motoctl_pwm.Moto(6, 13, 19, 26, 21, 20)
+
 @app.route('/')
 def index():
     return render_template("main.html")
@@ -10,19 +13,19 @@ def index():
 def control():
     if request.form["direction"] == "forward":
         print "forward"
-        moto.forward()
+        moto.forward()				# you can also call the function like 'moto.forward(dc)' to custom duty cycle.(0.0 <= dc <= 100.0, default 50)
         return "car forward"
     elif request.form["direction"] == "backward":
         print "backward"
-        moto.backward()
+        moto.backward()				# you can also call the function like 'moto.backward(dc)' to custom duty cycle.(0.0 <= dc <= 100.0, default 50)
         return "car backward"
     elif request.form["direction"] == "left":
         print "left"
-        moto.left()
+        moto.left()				# you can also call the function like 'moto.left(dc)' to custom duty cycle.(0.0 <= dc <= 100.0, default 50)
         return "car left"
     elif request.form["direction"] == "right":
         print "right"
-        moto.right()
+        moto.right()				# you can also call the function like 'moto.right(dc)' to custom duty cycle.(0.0 <= dc <= 100.0, default 50)
         return "car right"
     elif request.form["direction"] == "stop":
         print "stop"
